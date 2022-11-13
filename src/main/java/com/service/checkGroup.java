41b3dd63f0db1cb4a7970361e2ce2a39699725f9
liuqi <386269399@qq.com>
2022-11-13 18:05:02 +0800

    add checkGroup.class3

--- src/main/java/com/service/checkGroup.java
+++ src/main/java/com/service/checkGroup.java
@@ -2,7 +2,8 @@ package com.service;
 
 public class checkGroup {
     public static void main(String[] args) {
-        System.out.println("");
+        System.out.println("b");
+        System.out.println("c");
     }
 
 }
